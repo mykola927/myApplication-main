diem/diem

[move-package] Add package resolution and tests

language/tools/move-build/src/main.rs
@@ -1,14 +1,11 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use move_build::source_package::{layout, manifest_parser};
+use move_build::BuildConfig;
+use structopt::StructOpt;
 
-// TODO: This currently only parses the "Move.toml" under the current directory
 fn main() {
     let current_dir = std::env::current_dir().unwrap();
-    let manifest_string =
-        std::fs::read_to_string(current_dir.join(layout::SourcePackageLayout::Manifest.path()))
-            .unwrap();
-    let toml_manifest = manifest_parser::parse_move_manifest_string(manifest_string).unwrap();
-    manifest_parser::parse_source_manifest(toml_manifest).unwrap();
+    let build_config = BuildConfig::from_args();
+    build_config.build(&current_dir).unwrap();
 }

language/tools/move-build/tests/test_runner.rs
@@ -1,20 +1,50 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use move_build::source_package::manifest_parser as MP;
+use move_build::{
+    resolution::resolution_graph as RG, source_package::manifest_parser as MP, BuildConfig,
+};
 use move_command_line_common::testing::{format_diff, read_env_update_baseline, EXP_EXT};
-use std::{fs, path::Path};
+use std::{
+    ffi::OsStr,
+    fs,
+    path::{Component, Path, PathBuf},
+};
 
 pub fn run_test(path: &Path) -> datatest_stable::Result<()> {
     let update_baseline = read_env_update_baseline();
+    if path
+        .components()
+        .any(|component| component == Component::Normal(OsStr::new("deps_only")))
+    {
+        return Ok(());
+    }
     let exp_path = path.with_extension(EXP_EXT);
 
     let exp_exists = exp_path.is_file();
 
     let contents = fs::read_to_string(path)?;
-    let output = match MP::parse_move_manifest_string(contents).and_then(MP::parse_source_manifest)
+    let output = match MP::parse_move_manifest_string(contents)
+        .and_then(MP::parse_source_manifest)
+        .and_then(|parsed_manifest| {
+            RG::ResolutionGraph::new(
+                parsed_manifest,
+                path.parent().unwrap().to_path_buf(),
+                BuildConfig {
+                    dev_mode: true,
+                    generate_transaction_builders: false,
+                    generate_abis: false,
+                },
+            )
+        })
+        .and_then(|rg| rg.resolve())
     {
-        Ok(parsed_package) => format!("{:#?}", parsed_package),
+        Ok(mut resolved_package) => {
+            for (_, package) in resolved_package.package_table.iter_mut() {
+                package.package_path = PathBuf::from("ELIDED_FOR_TEST");
+            }
+            format!("{:#?}", resolved_package)
+        }
         Err(error) => format!("{:#}", error),
     };
 

mykola927/myApplication-main

language/tools/move-package/src/main.rs
@@ -1,14 +1,11 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use move_package::source_package::{layout, manifest_parser};
+use move_package::BuildConfig;
+use structopt::StructOpt;
 
-// TODO: This currently only parses the "Move.toml" under the current directory
 fn main() {
     let current_dir = std::env::current_dir().unwrap();
-    let manifest_string =
-        std::fs::read_to_string(current_dir.join(layout::SourcePackageLayout::Manifest.path()))
-            .unwrap();
-    let toml_manifest = manifest_parser::parse_move_manifest_string(manifest_string).unwrap();
-    manifest_parser::parse_source_manifest(toml_manifest).unwrap();
+    let build_config = BuildConfig::from_args();
+    build_config.build(&current_dir).unwrap();
 }

language/tools/move-package/tests/test_runner.rs
@@ -2,19 +2,49 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use move_command_line_common::testing::{format_diff, read_env_update_baseline, EXP_EXT};
-use move_package::source_package::manifest_parser as MP;
-use std::{fs, path::Path};
+use move_package::{
+    resolution::resolution_graph as RG, source_package::manifest_parser as MP, BuildConfig,
+};
+use std::{
+    ffi::OsStr,
+    fs,
+    path::{Component, Path, PathBuf},
+};
 
 pub fn run_test(path: &Path) -> datatest_stable::Result<()> {
     let update_baseline = read_env_update_baseline();
+    if path
+        .components()
+        .any(|component| component == Component::Normal(OsStr::new("deps_only")))
+    {
+        return Ok(());
+    }
     let exp_path = path.with_extension(EXP_EXT);
 
     let exp_exists = exp_path.is_file();
 
     let contents = fs::read_to_string(path)?;
-    let output = match MP::parse_move_manifest_string(contents).and_then(MP::parse_source_manifest)
+    let output = match MP::parse_move_manifest_string(contents)
+        .and_then(MP::parse_source_manifest)
+        .and_then(|parsed_manifest| {
+            RG::ResolutionGraph::new(
+                parsed_manifest,
+                path.parent().unwrap().to_path_buf(),
+                BuildConfig {
+                    dev_mode: true,
+                    generate_transaction_builders: false,
+                    generate_abis: false,
+                },
+            )
+        })
+        .and_then(|rg| rg.resolve())
     {
-        Ok(parsed_package) => format!("{:#?}\n", parsed_package),
+        Ok(mut resolved_package) => {
+            for (_, package) in resolved_package.package_table.iter_mut() {
+                package.package_path = PathBuf::from("ELIDED_FOR_TEST");
+            }
+            format!("{:#?}\n", resolved_package)
+        }
         Err(error) => format!("{:#}\n", error),
     };
 