gregnazario/diem

[network] Add NetworkInterface abstraction

network/src/application/mod.rs
@@ -1,0 +1,9 @@
+// Copyright (c) The Diem Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod db;
+pub mod interface;
+pub mod management;
+#[cfg(test)]
+mod tests;
+pub mod types;

mykola927/myApplication-main

network/src/application/mod.rs
@@ -1,0 +1,9 @@
+// Copyright (c) The Diem Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod interface;
+pub mod management;
+pub mod storage;
+#[cfg(test)]
+mod tests;
+pub mod types;