aptos-labs/aptos-core

[Aptos Compression] Add a simple compression crate.

crates/aptos-compression/src/lib.rs
@@ -1,0 +1,108 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::metrics::{
+    increment_compression_byte_count, increment_compression_error,
+    start_compression_operation_timer, COMPRESS, COMPRESSED_BYTES, DECOMPRESS, RAW_BYTES,
+};
+use aptos_logger::prelude::*;
+use lz4::block::CompressionMode;
+use thiserror::Error;
+
+/// This crate provides a simple library interface for data compression.
+/// It is useful for compressing large data chunks that are
+/// sent across the network (e.g., by state sync and consensus).
+/// Internally, it uses LZ4 in fast mode to compress the data.
+/// See https://github.com/10xGenomics/lz4-rs for more information.
+///
+/// Note: the crate also exposes some basic compression metrics
+/// that can be used to track the cumulative compression ratio
+/// and compression/decompression durations during the runtime.
+mod metrics;
+#[cfg(test)]
+mod tests;
+
+/// The acceleration parameter to use for FAST compression mode.
+/// This was determined anecdotally.
+const ACCELERATION_PARAMETER: i32 = 1;
+
+/// A useful wrapper for representing compressed data
+pub type CompressedData = Vec<u8>;
+
+/// An error type for capturing compression/decompression failures
+#[derive(Clone, Debug, Error)]
+#[error("Encountered a compression error! Error: {0}")]
+pub struct CompressionError(String);
+
+/// Compresses the raw data stream
+pub fn compress_data(raw_data: Vec<u8>) -> Result<CompressedData, CompressionError> {
+    // Start the compression timer
+    let timer = start_compression_operation_timer(COMPRESS);
+
+    // Compress the data
+    let compression_mode = CompressionMode::FAST(ACCELERATION_PARAMETER);
+    let compressed_data = match lz4::block::compress(&raw_data, Some(compression_mode), true) {
+        Ok(compressed_data) => compressed_data,
+        Err(error) => {
+            increment_compression_error(COMPRESS);
+            return Err(CompressionError(format!(
+                "Failed to compress the data: {:?}",
+                error.to_string()
+            )));
+        }
+    };
+
+    // Stop the timer and update the metrics
+    let compression_duration = timer.stop_and_record();
+    increment_compression_byte_count(RAW_BYTES, raw_data.len() as u64);
+    increment_compression_byte_count(COMPRESSED_BYTES, compressed_data.len() as u64);
+
+    // Log the relative data compression statistics
+    let relative_data_size = calculate_relative_size(&raw_data, &compressed_data);
+    trace!(
+        "Compressed {:?} bytes to {:?} bytes ({:?} %) in {:?} seconds.",
+        raw_data.len(),
+        compressed_data.len(),
+        relative_data_size,
+        compression_duration
+    );
+
+    Ok(compressed_data)
+}
+
+/// Decompresses the compressed data stream
+pub fn decompress_data(compressed_data: &CompressedData) -> Result<Vec<u8>, CompressionError> {
+    // Start the decompression timer
+    let timer = start_compression_operation_timer(DECOMPRESS);
+
+    // Decompress the data
+    let raw_data = match lz4::block::decompress(compressed_data, None) {
+        Ok(raw_data) => raw_data,
+        Err(error) => {
+            increment_compression_error(DECOMPRESS);
+            return Err(CompressionError(format!(
+                "Failed to decompress the data: {:?}",
+                error.to_string()
+            )));
+        }
+    };
+
+    // Stop the timer and log the relative data compression statistics
+    let decompression_duration = timer.stop_and_record();
+    let relative_data_size = calculate_relative_size(compressed_data, &raw_data);
+    trace!(
+        "Decompressed {:?} bytes to {:?} bytes ({:?} %) in {:?} seconds.",
+        compressed_data.len(),
+        raw_data.len(),
+        relative_data_size,
+        decompression_duration
+    );
+
+    Ok(raw_data)
+}
+
+/// Calculates the relative size (%) between the input and output after a
+/// compression/decompression operation, i.e., (output / input) * 100.
+fn calculate_relative_size(input: &Vec<u8>, output: &Vec<u8>) -> f64 {
+    (output.len() as f64 / input.len() as f64) * 100.0
+}

crates/aptos-compression/src/tests.rs
@@ -1,0 +1,156 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+use aptos_crypto::ed25519::Ed25519PrivateKey;
+use aptos_crypto::hash::HashValue;
+use aptos_crypto::{PrivateKey, SigningKey, Uniform};
+use aptos_types::account_address::AccountAddress;
+use aptos_types::chain_id::ChainId;
+use aptos_types::ledger_info::LedgerInfoWithSignatures;
+use aptos_types::transaction::{
+    ExecutionStatus, RawTransaction, Script, SignedTransaction, Transaction,
+    TransactionListWithProof, TransactionOutput, TransactionOutputListWithProof,
+    TransactionPayload, TransactionStatus,
+};
+use aptos_types::write_set::WriteSet;
+use aptos_types::{block_info::BlockInfo, ledger_info::LedgerInfo};
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use std::collections::BTreeMap;
+use std::fmt::Debug;
+
+#[test]
+fn test_basic_compression() {
+    // Test epoch ending ledger infos
+    let epoch_ending_ledger_infos = create_epoch_ending_ledger_infos(0, 999);
+    test_compress_and_decompress(epoch_ending_ledger_infos);
+
+    // Test transaction outputs with proof
+    let outputs_with_proof = create_output_list_with_proof(13434, 17000, 19000);
+    test_compress_and_decompress(outputs_with_proof);
+
+    // Test transactions with proof
+    let transactions_with_proof = create_transaction_list_with_proof(1000, 1999, 1999, true);
+    test_compress_and_decompress(transactions_with_proof);
+}
+
+/// Ensures that the given object can be compressed and decompressed successfully
+/// when BCS encoded.
+fn test_compress_and_decompress<T: Debug + DeserializeOwned + PartialEq + Serialize>(object: T) {
+    let bcs_encoded_bytes = bcs::to_bytes(&object).unwrap();
+    let compressed_bytes = crate::compress_data(bcs_encoded_bytes).unwrap();
+    let decompressed_bytes = crate::decompress_data(&compressed_bytes).unwrap();
+    let decoded_object = bcs::from_bytes::<T>(&decompressed_bytes).unwrap();
+
+    assert_eq!(object, decoded_object);
+}
+
+/// Creates a test epoch change proof
+fn create_epoch_ending_ledger_infos(
+    start_epoch: u64,
+    end_epoch: u64,
+) -> Vec<LedgerInfoWithSignatures> {
+    let mut ledger_info_with_sigs = vec![];
+    for epoch in start_epoch..end_epoch {
+        ledger_info_with_sigs.push(create_test_ledger_info_with_sigs(epoch, 0));
+    }
+    ledger_info_with_sigs
+}
+
+/// Creates a test transaction output list with proof
+fn create_output_list_with_proof(
+    start_version: u64,
+    end_version: u64,
+    proof_version: u64,
+) -> TransactionOutputListWithProof {
+    let transaction_list_with_proof =
+        create_transaction_list_with_proof(start_version, end_version, proof_version, false);
+    let transactions_and_outputs = transaction_list_with_proof
+        .transactions
+        .iter()
+        .map(|txn| (txn.clone(), create_test_transaction_output()))
+        .collect();
+
+    TransactionOutputListWithProof::new(
+        transactions_and_outputs,
+        Some(start_version),
+        transaction_list_with_proof.proof,
+    )
+}
+
+/// Creates a test ledger info with signatures
+fn create_test_ledger_info_with_sigs(epoch: u64, version: u64) -> LedgerInfoWithSignatures {
+    // Create a mock ledger info with signatures
+    let ledger_info = LedgerInfo::new(
+        BlockInfo::new(
+            epoch,
+            0,
+            HashValue::zero(),
+            HashValue::zero(),
+            version,
+            0,
+            None,
+        ),
+        HashValue::zero(),
+    );
+    LedgerInfoWithSignatures::new(ledger_info, BTreeMap::new())
+}
+
+/// Creates a test transaction output
+fn create_test_transaction_output() -> TransactionOutput {
+    TransactionOutput::new(
+        WriteSet::default(),
+        vec![],
+        0,
+        TransactionStatus::Keep(ExecutionStatus::MiscellaneousError(None)),
+    )
+}
+
+/// Creates a test user transaction
+fn create_test_transaction(sequence_number: u64) -> Transaction {
+    let private_key = Ed25519PrivateKey::generate_for_testing();
+    let public_key = private_key.public_key();
+
+    let transaction_payload = TransactionPayload::Script(Script::new(vec![], vec![], vec![]));
+    let raw_transaction = RawTransaction::new(
+        AccountAddress::random(),
+        sequence_number,
+        transaction_payload,
+        0,
+        0,
+        0,
+        ChainId::new(10),
+    );
+    let signed_transaction = SignedTransaction::new(
+        raw_transaction.clone(),
+        public_key,
+        private_key.sign(&raw_transaction),
+    );
+
+    Transaction::UserTransaction(signed_transaction)
+}
+
+/// Creates a test transaction output list with proof
+fn create_transaction_list_with_proof(
+    start_version: u64,
+    end_version: u64,
+    _proof_version: u64,
+    include_events: bool,
+) -> TransactionListWithProof {
+    // Include events if required
+    let events = if include_events { Some(vec![]) } else { None };
+
+    // Create the requested transactions
+    let mut transactions = vec![];
+    for sequence_number in start_version..=end_version {
+        transactions.push(create_test_transaction(sequence_number));
+    }
+
+    // Create a transaction list with an empty proof
+    let mut transaction_list_with_proof = TransactionListWithProof::new_empty();
+    transaction_list_with_proof.first_transaction_version = Some(start_version);
+    transaction_list_with_proof.events = events;
+    transaction_list_with_proof.transactions = transactions;
+
+    transaction_list_with_proof
+}

mykola927/myApplication-main

crates/aptos-compression/src/lib.rs
@@ -1,0 +1,108 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::metrics::{
+    increment_compression_byte_count, increment_compression_error,
+    start_compression_operation_timer, COMPRESS, COMPRESSED_BYTES, DECOMPRESS, RAW_BYTES,
+};
+use aptos_logger::prelude::*;
+use lz4::block::CompressionMode;
+use thiserror::Error;
+
+/// This crate provides a simple library interface for data compression.
+/// It is useful for compressing large data chunks that are
+/// sent across the network (e.g., by state sync and consensus).
+/// Internally, it uses LZ4 in fast mode to compress the data.
+/// See https://github.com/10xGenomics/lz4-rs for more information.
+///
+/// Note: the crate also exposes some basic compression metrics
+/// that can be used to track the cumulative compression ratio
+/// and compression/decompression durations during the runtime.
+mod metrics;
+#[cfg(test)]
+mod tests;
+
+/// The acceleration parameter to use for FAST compression mode.
+/// This was determined anecdotally.
+const ACCELERATION_PARAMETER: i32 = 1;
+
+/// A useful wrapper for representing compressed data
+pub type CompressedData = Vec<u8>;
+
+/// An error type for capturing compression/decompression failures
+#[derive(Clone, Debug, Error)]
+#[error("Encountered a compression error! Error: {0}")]
+pub struct CompressionError(String);
+
+/// Compresses the raw data stream
+pub fn compress(raw_data: Vec<u8>) -> Result<CompressedData, CompressionError> {
+    // Start the compression timer
+    let timer = start_compression_operation_timer(COMPRESS);
+
+    // Compress the data
+    let compression_mode = CompressionMode::FAST(ACCELERATION_PARAMETER);
+    let compressed_data = match lz4::block::compress(&raw_data, Some(compression_mode), true) {
+        Ok(compressed_data) => compressed_data,
+        Err(error) => {
+            increment_compression_error(COMPRESS);
+            return Err(CompressionError(format!(
+                "Failed to compress the data: {:?}",
+                error.to_string()
+            )));
+        }
+    };
+
+    // Stop the timer and update the metrics
+    let compression_duration = timer.stop_and_record();
+    increment_compression_byte_count(RAW_BYTES, raw_data.len() as u64);
+    increment_compression_byte_count(COMPRESSED_BYTES, compressed_data.len() as u64);
+
+    // Log the relative data compression statistics
+    let relative_data_size = calculate_relative_size(&raw_data, &compressed_data);
+    trace!(
+        "Compressed {:?} bytes to {:?} bytes ({:?} %) in {:?} seconds.",
+        raw_data.len(),
+        compressed_data.len(),
+        relative_data_size,
+        compression_duration
+    );
+
+    Ok(compressed_data)
+}
+
+/// Decompresses the compressed data stream
+pub fn decompress(compressed_data: &CompressedData) -> Result<Vec<u8>, CompressionError> {
+    // Start the decompression timer
+    let timer = start_compression_operation_timer(DECOMPRESS);
+
+    // Decompress the data
+    let raw_data = match lz4::block::decompress(compressed_data, None) {
+        Ok(raw_data) => raw_data,
+        Err(error) => {
+            increment_compression_error(DECOMPRESS);
+            return Err(CompressionError(format!(
+                "Failed to decompress the data: {:?}",
+                error.to_string()
+            )));
+        }
+    };
+
+    // Stop the timer and log the relative data compression statistics
+    let decompression_duration = timer.stop_and_record();
+    let relative_data_size = calculate_relative_size(compressed_data, &raw_data);
+    trace!(
+        "Decompressed {:?} bytes to {:?} bytes ({:?} %) in {:?} seconds.",
+        compressed_data.len(),
+        raw_data.len(),
+        relative_data_size,
+        decompression_duration
+    );
+
+    Ok(raw_data)
+}
+
+/// Calculates the relative size (%) between the input and output after a
+/// compression/decompression operation, i.e., (output / input) * 100.
+fn calculate_relative_size(input: &Vec<u8>, output: &Vec<u8>) -> f64 {
+    (output.len() as f64 / input.len() as f64) * 100.0
+}

crates/aptos-compression/src/tests.rs
@@ -1,0 +1,156 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+use aptos_crypto::ed25519::Ed25519PrivateKey;
+use aptos_crypto::hash::HashValue;
+use aptos_crypto::{PrivateKey, SigningKey, Uniform};
+use aptos_types::account_address::AccountAddress;
+use aptos_types::chain_id::ChainId;
+use aptos_types::ledger_info::LedgerInfoWithSignatures;
+use aptos_types::transaction::{
+    ExecutionStatus, RawTransaction, Script, SignedTransaction, Transaction,
+    TransactionListWithProof, TransactionOutput, TransactionOutputListWithProof,
+    TransactionPayload, TransactionStatus,
+};
+use aptos_types::write_set::WriteSet;
+use aptos_types::{block_info::BlockInfo, ledger_info::LedgerInfo};
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use std::collections::BTreeMap;
+use std::fmt::Debug;
+
+#[test]
+fn test_basic_compression() {
+    // Test epoch ending ledger infos
+    let epoch_ending_ledger_infos = create_epoch_ending_ledger_infos(0, 999);
+    test_compress_and_decompress(epoch_ending_ledger_infos);
+
+    // Test transaction outputs with proof
+    let outputs_with_proof = create_output_list_with_proof(13434, 17000, 19000);
+    test_compress_and_decompress(outputs_with_proof);
+
+    // Test transactions with proof
+    let transactions_with_proof = create_transaction_list_with_proof(1000, 1999, 1999, true);
+    test_compress_and_decompress(transactions_with_proof);
+}
+
+/// Ensures that the given object can be compressed and decompressed successfully
+/// when BCS encoded.
+fn test_compress_and_decompress<T: Debug + DeserializeOwned + PartialEq + Serialize>(object: T) {
+    let bcs_encoded_bytes = bcs::to_bytes(&object).unwrap();
+    let compressed_bytes = crate::compress(bcs_encoded_bytes).unwrap();
+    let decompressed_bytes = crate::decompress(&compressed_bytes).unwrap();
+    let decoded_object = bcs::from_bytes::<T>(&decompressed_bytes).unwrap();
+
+    assert_eq!(object, decoded_object);
+}
+
+/// Creates a test epoch change proof
+fn create_epoch_ending_ledger_infos(
+    start_epoch: u64,
+    end_epoch: u64,
+) -> Vec<LedgerInfoWithSignatures> {
+    let mut ledger_info_with_sigs = vec![];
+    for epoch in start_epoch..end_epoch {
+        ledger_info_with_sigs.push(create_test_ledger_info_with_sigs(epoch, 0));
+    }
+    ledger_info_with_sigs
+}
+
+/// Creates a test transaction output list with proof
+fn create_output_list_with_proof(
+    start_version: u64,
+    end_version: u64,
+    proof_version: u64,
+) -> TransactionOutputListWithProof {
+    let transaction_list_with_proof =
+        create_transaction_list_with_proof(start_version, end_version, proof_version, false);
+    let transactions_and_outputs = transaction_list_with_proof
+        .transactions
+        .iter()
+        .map(|txn| (txn.clone(), create_test_transaction_output()))
+        .collect();
+
+    TransactionOutputListWithProof::new(
+        transactions_and_outputs,
+        Some(start_version),
+        transaction_list_with_proof.proof,
+    )
+}
+
+/// Creates a test ledger info with signatures
+fn create_test_ledger_info_with_sigs(epoch: u64, version: u64) -> LedgerInfoWithSignatures {
+    // Create a mock ledger info with signatures
+    let ledger_info = LedgerInfo::new(
+        BlockInfo::new(
+            epoch,
+            0,
+            HashValue::zero(),
+            HashValue::zero(),
+            version,
+            0,
+            None,
+        ),
+        HashValue::zero(),
+    );
+    LedgerInfoWithSignatures::new(ledger_info, BTreeMap::new())
+}
+
+/// Creates a test transaction output
+fn create_test_transaction_output() -> TransactionOutput {
+    TransactionOutput::new(
+        WriteSet::default(),
+        vec![],
+        0,
+        TransactionStatus::Keep(ExecutionStatus::MiscellaneousError(None)),
+    )
+}
+
+/// Creates a test user transaction
+fn create_test_transaction(sequence_number: u64) -> Transaction {
+    let private_key = Ed25519PrivateKey::generate_for_testing();
+    let public_key = private_key.public_key();
+
+    let transaction_payload = TransactionPayload::Script(Script::new(vec![], vec![], vec![]));
+    let raw_transaction = RawTransaction::new(
+        AccountAddress::random(),
+        sequence_number,
+        transaction_payload,
+        0,
+        0,
+        0,
+        ChainId::new(10),
+    );
+    let signed_transaction = SignedTransaction::new(
+        raw_transaction.clone(),
+        public_key,
+        private_key.sign(&raw_transaction),
+    );
+
+    Transaction::UserTransaction(signed_transaction)
+}
+
+/// Creates a test transaction output list with proof
+fn create_transaction_list_with_proof(
+    start_version: u64,
+    end_version: u64,
+    _proof_version: u64,
+    include_events: bool,
+) -> TransactionListWithProof {
+    // Include events if required
+    let events = if include_events { Some(vec![]) } else { None };
+
+    // Create the requested transactions
+    let mut transactions = vec![];
+    for sequence_number in start_version..=end_version {
+        transactions.push(create_test_transaction(sequence_number));
+    }
+
+    // Create a transaction list with an empty proof
+    let mut transaction_list_with_proof = TransactionListWithProof::new_empty();
+    transaction_list_with_proof.first_transaction_version = Some(start_version);
+    transaction_list_with_proof.events = events;
+    transaction_list_with_proof.transactions = transactions;
+
+    transaction_list_with_proof
+}